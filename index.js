3c9ac2f160e67bcd2d7c21eeac217438f9609dc4
Stanislav Sergienya <skilful522@gmail.com>
2019-09-26 09:41:32 +0300

    minor fix

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const addButton = document.querySelector("#addButton");
+
 const textInput = document.querySelector('#textInput');
+
 const dateInput = document.querySelector("#dateInput");
+
 const tasksContainer = document.querySelector('#tasks-container');
+
 const taskArr = [];
+
 const dateArr = [];
+
 const sortAlphabetButton = document.querySelector("#sortAlphabet");
+
 const sortDateButton = document.querySelector("#sortDate");
+
 let sortAlphabetCounter = 0;
+
 let sortDateCounter = 0;
+
 
 addButton.addEventListener('click', () => {
     addContainer();
@@ -16,15 +26,18 @@ addButton.addEventListener('click', () => {
 
 sortAlphabetButton.addEventListener('click', () => {
     const tasks = document.querySelectorAll('#task');
+
     if (taskArr.length > 1) {
         sortAlphabetCounter++;
         if (sortAlphabetCounter === 1) {
             const sortedTasks = taskArr.sort();
+
             for (let i = 0; i < tasks.length; i++) {
                 tasks[i].innerText = sortedTasks[i];
             }
         } else if (sortAlphabetCounter === 2) {
             const sortedReversedTasks = taskArr.sort().reverse();
+
             for (let i = 0; i < tasks.length; i++) {
                 tasks[i].innerText = sortedReversedTasks[i];
                 sortAlphabetCounter = 0;
@@ -36,12 +49,14 @@ sortAlphabetButton.addEventListener('click', () => {
 
 sortDateButton.addEventListener('click', () => {
     const dates = document.querySelectorAll('#date');
+
     if (dateArr.length > 1) {
         sortDateCounter++;
         if (sortDateCounter === 1) {
             const sortedDates = dateArr.sort(function (first, second) {
                 return new Date(second) - new Date(first);
             });
+
             for (let i = 0; i < dates.length; i++) {
                 dates[i].innerText = sortedDates[i];
             }
@@ -49,6 +64,7 @@ sortDateButton.addEventListener('click', () => {
             const reversedSortedDates = dateArr.sort(function (first, second) {
                 return new Date(first) - new Date(second);
             });
+
             for (let i = 0; i < dates.length; i++) {
                 dates[i].innerText = reversedSortedDates[i];
             }
@@ -58,66 +74,15 @@ sortDateButton.addEventListener('click', () => {
 
 });
 
-function cleanInputs() {
-    textInput.value = '';
-    dateInput.value = '';
-}
-
-function createContainer(className, id) {
-    const container = document.createElement('div');
-    container.className = className;
-    container.id = id;
-    return container;
-}
-
-
-function createButton(id, innerText, color) {
-    const button = document.createElement('button');
-    button.id = id;
-    button.innerText = innerText;
-    button.style.color = color;
-    return button;
-}
-
-function checkTextInput(textInput, divText) {
-    if (textInput.value !== '') {
-        divText.innerText = textInput.value;
-    }
-}
-
-function createTaskDiv() {
-    const taskDiv = document.createElement('div');
-    taskDiv.id = 'task';
-    checkTextInput(textInput, taskDiv);
-    taskArr.push(taskDiv.innerText);
-    return taskDiv;
-}
-
-function createDateDiv() {
-    const dateDiv = document.createElement('div');
-    const date = moment().format('YYYY-MM-DD');
-    dateDiv.id = 'date';
-    dateDiv.innerText = dateInput.value;
-    if (dateDiv.innerText === '') {
-        dateDiv.innerText = date;
-    }
-    dateArr.push(dateDiv.innerText);
-    return dateDiv;
-}
-
-function addContainer() {
-    const task = addElementIntoContainer();
-    if (task !== undefined) {
-        tasksContainer.appendChild(task);
-    }
-
-}
-
 tasksContainer.addEventListener('click', (event) => {
     const taskContainer = document.querySelectorAll('.task-container');
+
     const delButtons = document.querySelectorAll('#delButton');
+
     const doneButtons = document.querySelectorAll('#doneButton');
+
     const taskDateContainer = document.querySelectorAll(".task-date-container");
+
     for (let i = 0; i < delButtons.length; i++) {
         if (event.target === delButtons[i]) {
             for (let i = 0; i < taskContainer.length; i++) {
@@ -134,13 +99,80 @@ tasksContainer.addEventListener('click', (event) => {
 
 });
 
+function cleanInputs() {
+    textInput.value = '';
+    dateInput.value = '';
+}
+
+function createContainer(className, id) {
+    const container = document.createElement('div');
+
+    container.className = className;
+    container.id = id;
+    return container;
+}
+
+
+function createButton(id, innerText, color) {
+    const button = document.createElement('button');
+
+    button.id = id;
+    button.innerText = innerText;
+    button.style.color = color;
+    return button;
+}
+
+function checkTextInput(textInput, divText) {
+    if (textInput.value !== '') {
+        divText.innerText = textInput.value;
+    }
+}
+
+function createTaskDiv() {
+    const taskDiv = document.createElement('div');
+
+    taskDiv.id = 'task';
+    checkTextInput(textInput, taskDiv);
+    taskArr.push(taskDiv.innerText);
+    return taskDiv;
+}
+
+function createDateDiv() {
+    const dateDiv = document.createElement('div');
+
+    const date = moment().format('YYYY-MM-DD');
+
+    dateDiv.id = 'date';
+    dateDiv.innerText = dateInput.value;
+    if (dateDiv.innerText === '') {
+        dateDiv.innerText = date;
+    }
+    dateArr.push(dateDiv.innerText);
+    return dateDiv;
+}
+
+function addContainer() {
+    const task = addElementIntoContainer();
+
+    if (task !== undefined) {
+        tasksContainer.appendChild(task);
+    }
+
+}
+
 function addElementIntoContainer() {
     const id = Date.now();
+
     const container = createContainer('task-container', id);
+
     const taskDateContainer = createContainer('task-date-container', id);
+
     const delBtn = createButton('delButton', '✖', 'red');
+
     const doneBtn = createButton('doneButton', '✓', 'green');
+
     const taskDiv = createTaskDiv();
+
     const timeDiv = createDateDiv();
 
     taskDateContainer.appendChild(taskDiv);
